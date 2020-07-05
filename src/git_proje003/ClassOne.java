d50857afc3015223aef8a94a4e731276adbf9b58
Halil Ibrahim <hisetkal@gmail.com>
2020-07-05 14:06:23 -0400

    ucuncu commit yapildi

--- src/git_proje003/ClassOne.java
+++ src/git_proje003/ClassOne.java
@@ -6,5 +6,7 @@ public static void main(String[] args) {
 	
 	System.out.println("local master branch");
 	System.out.println("loacal master branch 2. commit");
+	System.out.println("local master 3. commit");
+	System.out.println("loacal master 3. commit");
 }
 }
