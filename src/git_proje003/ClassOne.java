0edaa8980082529514240c5eee11e1ba0dd6eaf0
Halil Ibrahim <hisetkal@gmail.com>
2020-07-05 14:17:21 -0400

    4. commit masterdan

--- src/git_proje003/ClassOne.java
+++ src/git_proje003/ClassOne.java
@@ -13,6 +13,7 @@ public static void main(String[] args) {
 	
 	System.out.println("remote master branch");
 	System.out.println("remote master branch 1. commit");
+	System.out.println("israrla burada masterdan conflict");
 
 }
 }
