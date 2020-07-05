5a34215e3308ff30da4f4dbc473826485a9d648e
Halil Ibrahim <46765709+HalilIbrahimHasan@users.noreply.github.com>
2020-07-05 14:04:25 -0400

    Update ClassOne.java
    degisiklikler remote masterdan yapilmistir

--- src/git_proje003/ClassOne.java
+++ src/git_proje003/ClassOne.java
@@ -6,5 +6,8 @@ public static void main(String[] args) {
 	
 	System.out.println("local master branch");
 	System.out.println("loacal master branch 2. commit");
+	
+	System.out.println("local master branch");
+	System.out.println("loacal master branch 2. commit");
 }
 }
