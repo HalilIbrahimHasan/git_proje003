8348930c2dae4c3f9d9de1e4d67cce4229de1e47
Halil Ibrahim <hisetkal@gmail.com>
2020-07-05 14:15:46 -0400

    ilk commit feature branchten

--- src/git_proje003/ClassOne.java
+++ src/git_proje003/ClassOne.java
@@ -13,6 +13,8 @@ public static void main(String[] args) {
 	
 	System.out.println("remote master branch");
 	System.out.println("remote master branch 1. commit");
+	System.out.println("local feature branch ilk commit");
+	
 
 }
 }
---
+++ src/git_proje003/FeatureClass.java
@@ -0,0 +1,7 @@
+package git_proje003;
+
+public class FeatureClass {
+public static void main(String[] args) {
+	
+}
+}
